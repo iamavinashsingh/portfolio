be3300a03a83597ac66e7d3f0cc7fa8813ca9e94
Avinash Singh <imavinashsingh2@gmail.com>
2026-02-12 01:12:13 +0530

    Font Setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1 className='text-6xl text-red-500 font-extrabold'>Hemlo Avi</h1>
+      <h1 className='text-6xl  text-red-500 italic uppercase'>Hemlo Avi</h1>
+      <h1 className='text-6xl  text-red-500 font-bold uppercase'>Hemlo Avi</h1>
+      <h2 className='text-6xl  text-pink-500 font-extralight uppercase'>Hemlo Avi</h2>
     </div>
   )
 }
