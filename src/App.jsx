babd2bd32ceed73fc61b7429f7f2eacbc397c7c8
Avinash Singh <imavinashsingh2@gmail.com>
2026-02-11 23:13:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-black w-screen h-screen flex items-center justify-center'>
+      <h1 className="text-3xl text-red-400 font-bold underline">
+        Hello world!
+      </h1>
+
+    </div>
+  )
+}
+
+export default App
